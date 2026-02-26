c26719b697d8875db76507ddf1a58eb90c24067a
Ben Mbairo <ben-mbairo.allatessem@keyrus.com>
2026-02-26 22:28:03 +0100

    New tables from another sources

--- models/staging/linki/stg_interactions.sql
+++ models/staging/linki/stg_interactions.sql
@@ -1,4 +1,6 @@
 SELECT
-    *,
+    url_du_post,
+    date_de_publication_du_post,
+    interactions,
     CURRENT_TIMESTAMP() AS _at_load
-FROM {{ source('linki_bucket_set', 'interactions') }}
+FROM {{ source('google_drive', 'contenu_2025_01_21_2026_01_20_ben_mbairo_interactions') }}
