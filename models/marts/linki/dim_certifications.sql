bf3741e947740fef732652bae86028e9fe94f8a1
Ben Mbairo <ben-mbairo.allatessem@keyrus.com>
2026-03-22 15:17:42 +0100

    Remove some Ref tables that doesn't help for BI

--- models/marts/linki/dim_certifications.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    FARM_FINGERPRINT(CONCAT(name, COALESCE(authority, ''))) AS certification_id,
-    name,
-    url,
-    authority,
-    started_on,
-    finished_on,
-    license_number
-FROM {{ ref('int_certifications') }}
