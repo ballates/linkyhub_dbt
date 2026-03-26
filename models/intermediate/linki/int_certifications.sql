832b35f2380dd2699593820d857783b0ee086ce6
Ben Mbairo <ben-mbairo.allatessem@keyrus.com>
2026-03-26 23:12:31 +0100

    Refactore the project

--- models/intermediate/linki/int_certifications.sql
+++ models/intermediate/linki/int_certifications.sql
@@ -1,9 +1,9 @@
 SELECT
-    Name                                         AS name,
-    Url                                          AS url,
-    Authority                                    AS authority,
-    SAFE.PARSE_TIMESTAMP('%b %Y', `Started On`)  AS started_on,
-    SAFE.PARSE_TIMESTAMP('%b %Y', `Finished On`) AS finished_on,
-    `License Number`                             AS license_number,
-    CURRENT_TIMESTAMP()                          AS _at_load
+    name,
+    url,
+    authority,
+    started_on,
+    finished_on,
+    license_number,
+    CURRENT_TIMESTAMP()   AS _at_load
 FROM {{ ref('stg_certifications') }}
--- models/marts/linki/dim_calendar.sql
+++ models/marts/linki/dim_calendar.sql
@@ -1,5 +1,5 @@
 WITH date_spine AS (
-    {{ generate_date_spine('2021-01-01', '2026-12-31') }}
+    {{ generate_date_spine('2021-01-01', '2027-12-31') }}
 )
 
 SELECT
