efc594fd40f3f8cb6bef7ad1011ca1234fa39929
Ben Mbaïro <48687871+ballates@users.noreply.github.com>
2026-05-06 16:08:19 +0200

    Update Connections table (#3)
    * Update Connections table
    
    * Set te certifications columns

--- models/staging/linki/stg_learning.sql
+++ models/staging/linki/stg_learning.sql
@@ -8,26 +8,38 @@
     Aucune logique métier. Aucun filtre.
 */
 
+WITH renamed AS (
+    SELECT
+        string_field_0  AS title,
+        string_field_1  AS description,
+        string_field_2  AS type,
+        string_field_3  AS last_watched_date,
+        string_field_4  AS completed_at,
+        string_field_5  AS saved,
+        string_field_6  AS notes
+    FROM {{ source('linki_bucket_set', 'learning') }}
+    WHERE string_field_0 != 'Content Title'
+)
+
 SELECT
     -- ================================================================
     -- CLÉ TECHNIQUE / SURROGATE KEY
     -- ================================================================
-    {{ dbt_utils.generate_surrogate_key(['string_field_0']) }}  AS id_learning,
+    {{ dbt_utils.generate_surrogate_key(['title']) }}  AS id_learning,
 
     -- ================================================================
     -- COLONNES MÉTIER
     -- ================================================================
-    string_field_0      AS title,
-    string_field_1      AS description,
-    string_field_2      AS type,
-    string_field_3      AS last_watched_date,
-    string_field_4      AS completed_at,
-    string_field_5      AS saved,
-    string_field_6      AS notes,
-    string_field_7      AS field_extra,
+    title,
+    description,
+    type,
+    last_watched_date,
+    completed_at,
+    saved,
+    notes,
 
     -- ================================================================
     -- MÉTADONNÉES DE TRAÇABILITÉ
     -- ================================================================
     CURRENT_TIMESTAMP() AS _at_load
-FROM {{ source('linki_bucket_set', 'learning') }}
+FROM renamed
